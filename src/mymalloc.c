c89c57c7d11bd9eba13c6655bbcb4854fb89f1e8
marcuscv <77870374+marcuscv@users.noreply.github.com>
2022-02-17 00:42:18 -0500

    Update mymalloc.c

--- src/mymalloc.c
+++ src/mymalloc.c
@@ -8,12 +8,24 @@ typedef struct metadata{
 	int available;	
 }node;
 
+typedef struct node{
+	int data;
+	int key;
+	struct node *next;
+};
+
 static char mem[4096];
 
 void *mymalloc(size_t size, char *file, int line){
+	FILE *fp;
+	fp = fopen(*file, "r+");
 
 }
 
 void free(void *ptr, char *file, int line){
+	FILE *fp;
+	char f = *file;
+	fp = fopen(f, "r+");
+	scanf("%s", fp);
 
 }
